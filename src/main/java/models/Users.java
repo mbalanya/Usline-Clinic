e79204c5e0b7e6ae972e4466b87c31708e61d4b9
frank7841 <franklinewax@gmail.com>
2020-07-22 19:52:55 +0300

    created database sql and login class

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,10 +1,11 @@
 package models;
 
 public abstract class Users {
-    public String email;
-    public String password;
+
     public String firstName;
     public String lastName;
+    public String email;
+    public String password;
     public String gender;
     public String phone;
     public String role;
--- src/main/resources/db/craete.sql
+++ src/main/resources/db/craete.sql
@@ -1,1 +1,8 @@
-CREA
+CREATE DATABASE usline-_clinic;
+\c usline-clinic;
+CREATE TABLE userlogin(id serial PRIMARY KEY, username varchar, password varchar, role varchar);
+CREATE TABLE patient(id serial PRIMARY KEY, firstname varchar, lastname varchar, email varchar, password varchar, gendar varchar, phone varchar,role varchar);
+CREATE TABLE doctor(id serial PRIMARY KEY, firstname varchar, lastname varchar, email varchar, password varchar, gendar varchar, phone varchar,role varchar);
+CREATE TABLE appointment(id serial PRIMARY KEY, day timestamp,starttime timestamp,endtime timestamp, docid int, patid int);
+
+CREATE DATABASE usline_clinic_test WITH TEMPLATE usline_clinic;
