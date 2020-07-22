d35d4773e3c5946839cf16f3c12a7d973ab019f3
frank7841 <franklinewax@gmail.com>
2020-07-22 08:52:02 +0300

    Created class Users

---
+++ src/main/java/models/Users.java
@@ -0,0 +1,12 @@
+package models;
+
+public abstract class Users {
+    public int userid;
+    public String email;
+    public String password;
+    public String firstName;
+    public String lastName;
+    public String gender;
+    public String phone;
+    public String role;
+}
