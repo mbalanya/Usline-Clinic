7b00c3ac2a11179bd3ecfc98e2a0d082f70d3ea0
frank7841 <franklinewax@gmail.com>
2020-07-22 20:00:48 +0300

    overiding equals and hashcode

--- src/main/java/models/Login.java
+++ src/main/java/models/Login.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Login {
 private int id;
 private String username;
@@ -12,5 +14,62 @@ private int patid;
         this.password = password;
         this.docid = docid;
         this.patid = patid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDocid() {
+        return docid;
+    }
+
+    public void setDocid(int docid) {
+        this.docid = docid;
+    }
+
+    public int getPatid() {
+        return patid;
+    }
+
+    public void setPatid(int patid) {
+        this.patid = patid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Login login = (Login) o;
+        return id == login.id &&
+                docid == login.docid &&
+                patid == login.patid &&
+                username.equals(login.username) &&
+                password.equals(login.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, docid, patid);
     }
 }
