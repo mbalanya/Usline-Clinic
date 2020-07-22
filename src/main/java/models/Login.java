87c4a4af6282340278a25c95ba785e4a4a1129b3
frank7841 <franklinewax@gmail.com>
2020-07-22 19:59:15 +0300

    defining class login

--- src/main/java/models/Login.java
+++ src/main/java/models/Login.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Login {
+private int id;
+private String username;
+private String password;
+private int docid;
+private int patid;
 
+    public Login(String username, String password, int docid, int patid) {
+        this.username = username;
+        this.password = password;
+        this.docid = docid;
+        this.patid = patid;
+    }
 }
