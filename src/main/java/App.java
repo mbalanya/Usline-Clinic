324616a499368633df52e37f91e7e4d0c124c6e9
frank7841 <franklinewax@gmail.com>
2020-07-23 19:58:24 +0300

    Creating login route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,44 @@
-public class App {
+package dao;
+import dao.*;
+import com.google.gson.Gson;
+import models.Login;
+import org.sql2o.Sql2o;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args){
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+        Sql2o sql2o = DB.sql2o;
+        Connection con;
+        Gson gson=new Gson();
+        staticFileLocation("public");
+        Sql2oLoginDao loginDao;
+        loginDao = new Sql2oLoginDao(sql2o);
+
+        get("/login", "application/json", (req, res) -> {
+            Login login = gson.fromJson(req.body(), Login.class);
+            res.status(201);
+            return gson.toJson(login);
+        });
+        post("/login-details", "application/json", (req, res) -> {
+            Login login = gson.fromJson(req.body(), Login.class);
+            loginDao.add(login);
+            res.status(201);
+            return gson.toJson(login);
+        });
+    }
 }
