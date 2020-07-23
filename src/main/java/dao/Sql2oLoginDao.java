f8af05608f84dd94b3576f54ca5de94d9e8924e7
frank7841 <franklinewax@gmail.com>
2020-07-23 16:08:55 +0300

    created class DB and database connection

--- src/main/java/dao/Sql2oLoginDao.java
+++ src/main/java/dao/Sql2oLoginDao.java
@@ -10,20 +10,21 @@ import java.util.List;
 public class Sql2oLoginDao implements LoginDao{
     private final Sql2o sql2o;
     public Sql2oLoginDao(Sql2o sql2o){ this.sql2o = sql2o; }
+    
 
-    @Override
-    public void add(Login login) {
-        String sql = "INSERT INTO doctor (username, password, docid, patid) VALUES (:username, :password, :docid, :patid)";
-        try(Connection con = sql2o.open()){
-            int id = (int) con.createQuery(sql, true)
-                    .bind(login)
-                    .executeUpdate()
-                    .getKey();
-            login.setId(id);
-        } catch (Sql2oException ex) {
-            System.out.println(ex);
-        }
-    }
+//    @Override
+//    public void add(Login login) {
+//        String sql = "INSERT INTO doctor (username, password, docid, patid) VALUES (:username, :password, :docid, :patid)";
+//        try(Connection con = sql2o.open()){
+//            int id = (int) con.createQuery(sql, true)
+//                    .bind(login)
+//                    .executeUpdate()
+//                    .getKey();
+//            login.setId(id);
+//        } catch (Sql2oException ex) {
+//            System.out.println(ex);
+//        }
+//    }
 
     /*@Override
     public List<Login> getAll() {
@@ -33,26 +34,26 @@ public class Sql2oLoginDao implements LoginDao{
         }
     }*/
 
-    @Override
-    public Login findById(int id) {
-        try(Connection con = sql2o.open()){
-            return con.createQuery("SELECT * FROM login WHERE id = :id")
-                    .addParameter("id", id)
-                    .executeAndFetchFirst(Login.class);
-        }
-    }
+//    @Override
+//    public Login logIn(int id) {
+//        try(Connection con = sql2o.open()){
+//            return con.createQuery("SELECT * FROM login WHERE id = :id")
+//                    .addParameter("id", id)
+//                    .executeAndFetchFirst(Login.class);
+//        }
+//    }
 
-    @Override
-    public void deleteById(int id) {
-        String sql = "DELETE from login WHERE id = :id";
-        try(Connection con = sql2o.open()){
-            con.createQuery(sql)
-                    .addParameter("id", id)
-                    .executeUpdate();
-        } catch (Sql2oException ex) {
-            System.out.println(ex);
-        }
-    }
+//    @Override
+//    public void deleteById(int id) {
+//        String sql = "DELETE from login WHERE id = :id";
+//        try(Connection con = sql2o.open()){
+//            con.createQuery(sql)
+//                    .addParameter("id", id)
+//                    .executeUpdate();
+//        } catch (Sql2oException ex) {
+//            System.out.println(ex);
+//        }
+//    }
 
     /*@Override
     public void clearAll() {
