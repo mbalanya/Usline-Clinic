7a19d6d6fa7bd42d0e494638461fe7e1cebe8ea6
frank7841 <franklinewax@gmail.com>
2020-07-23 20:28:41 +0300

    Formatting create.sql

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,1 +1,9 @@
+CREATE DATABASE usline_clinic;
+\c usline_clinic;
+CREATE TABLE userlogin(id serial PRIMARY KEY, username varchar, password varchar, docid integer, patis integer);
+CREATE TABLE patient (id serial PRIMARY KEY, email varchar, firstname varchar, lastname varchar, gender varchar, phone varchar, role varchar patid integer);
+CREATE TABLE doctor (id serial PRIMARY KEY, email varchar, firstname varchar, lastname varchar, gender varchar, phone varchar, role varchar docid integer);
+CREATE TABLE appointment(id serial PRIMARY KEY, day timestamp, starttime timestamp, endtime timestamp patid integer, docid integer);
+CREATE DATABASE  usline_clinic_test WITH TEMPLATE usline_clinic;
+
 
