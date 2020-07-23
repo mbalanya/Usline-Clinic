4c60dcbc7b9a44664d26bbc332b79bd32a4b0f9a
frank7841 <franklinewax@gmail.com>
2020-07-23 20:18:55 +0300

    Modifying create.sql

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,7 +1,1 @@
-CREATE DATABASE usline_clinic;
-CREATE TABLE userlogin(id serial PRIMARY KEY, username varchar, password varchar, docid integer, patid integer );
-CREATE TABLE patient(id serial PRIMARY KEY, email varchar,firstname varchar, lastname varchar, gender varchar, phone varchar, role varchar);
-CREATE TABLE doctor(id serial PRIMARY KEY, email varchar,firstname varchar, lastname varchar, gender varchar, phone varchar, role varchar);
-CREATE TABLE appointment(id serial PRIMARY KEY, day timestamp, starttime timestamp, endtime timestamp docid integer, patid integer);
 
-CREATE DATABASE usline_clinic_test WITH TEMPLATE usline_clinic;
