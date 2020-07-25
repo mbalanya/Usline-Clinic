f419abbe8ef67cc7ce46379f4477bb25dfb83842
frank7841 <franklinewax@gmail.com>
2020-07-25 19:09:58 +0300

    working on appointmentdao.java interface

--- src/main/java/dao/AppointmentDao.java
+++ src/main/java/dao/AppointmentDao.java
@@ -1,4 +1,16 @@
 package dao;
 
+import models.Appointment;
+import models.Doctor;
+
+import java.util.List;
+
 public interface AppointmentDao {
+    void add(Appointment appointment);
+
+    List<Appointment> getAll();
+    Appointment findById(int id);
+
+    void deleteById(int id);
+    void clearAll();
 }
